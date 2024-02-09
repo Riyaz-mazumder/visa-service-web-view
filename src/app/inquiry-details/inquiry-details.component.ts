69a1cb87c8fa3240e44a52fdb2e7a80a10ee45f6
Riaj uddin <riajmazumder711@gmail.com>
2024-02-09 13:10:26 +0600

    adding inquiry-details design

--- src/app/inquiry-details/inquiry-details.component.ts
+++ src/app/inquiry-details/inquiry-details.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inquiry-details',
   templateUrl: './inquiry-details.component.html',
   styleUrls: ['./inquiry-details.component.scss']
 })
-export class InquiryDetailsComponent {
+export class InquiryDetailsComponent implements OnInit{
+     
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    this.setupTabs();
+  }
+
+  setupTabs() {
+    document.querySelectorAll('.tab-btn').forEach((button: Element) => {
+      button.addEventListener('click', () => {
+
+        const sidebar = button.parentElement as HTMLElement;
+        const tabs = sidebar.parentElement as HTMLElement;
+        const tabNumber = button.getAttribute('data-for-tab');
+        const tabActivate = tabs.querySelector(`.tab-content[data-tab="${tabNumber}"]`) as HTMLElement;
+
+        sidebar.querySelectorAll('.tab-btn').forEach((btn: Element) => {
+          btn.classList.remove('tab-btn-active');
+        });
+        tabs.querySelectorAll('.tab-content').forEach((tab: Element) => {
+          tab.classList.remove('tab-content-active');
+        });
+        button.classList.add('tab-btn-active');
+        tabActivate.classList.add('tab-content-active');
+      });
+    });
+  }
+
+  
 
 }
